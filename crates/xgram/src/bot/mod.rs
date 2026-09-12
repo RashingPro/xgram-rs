b80dec93b87961ccaf189fe938b44e55b3405cd4
RashingPro <rashingpro@yandex.ru>
2026-09-12 18:48:42 +0300

    refactor: move update handling to separate function

--- crates/xgram/src/bot/mod.rs
+++ crates/xgram/src/bot/mod.rs
@@ -6,7 +6,7 @@ use log::{error, trace};
 use std::sync::Arc;
 use xgram_telegram_api::client::TelegramApiClient;
 use xgram_telegram_api::types::message::entity::MessageEntityType;
-use xgram_telegram_api::types::update::UpdateKind;
+use xgram_telegram_api::types::update::{Update, UpdateKind};
 use xgram_telegram_api::update_receiver::UpdateReceiver;
 use xgram_telegram_api::update_receiver::http::HttpUpdateReceiver;
 use xgram_utils::config::BotConfig;
@@ -48,35 +48,46 @@ impl Bot {
         let update_receiver = HttpUpdateReceiver::new(self.config.clone(), self.client.clone());
         let mut update_receiver = update_receiver.spawn();
 
+        let slf = Arc::new(self);
         while let Some(update) = update_receiver.recv().await {
             match update {
                 Ok(update) => {
                     trace!(target: "main_loop", "Received update: {:#?}", update);
-                    if let UpdateKind::NewMessage(message) = update.update_kind
-                        && let Some(message_text) = &message.text
-                        && let Some(entities) = &message.entities
                     {
-                        for entity in entities {
-                            if let MessageEntityType::BotCommand = entity.entity_type {
-                                let command_text = &message_text[entity.offset as usize + 1
-                                    ..(entity.offset + entity.length) as usize];
-                                if let Some(command_handler) = self.commands.get(command_text) {
-                                    trace!(
-                                        "Handling command {}",
-                                        format!("/{}", command_text).yellow()
-                                    );
-                                    tokio::spawn(command_handler(CommandContext::new(
-                                        self.client.clone(),
-                                        message
-                                    )));
-                                    break;
-                                }
-                            }
-                        }
+                        let slf = slf.clone();
+                        tokio::spawn(async move { slf.handle_update(update).await });
                     }
                 }
                 Err(err) => error!(target: "main_loop", "{}", err)
             }
+        }
+    }
+
+    #[allow(clippy::single_match)] // TODO: remove when more update handlers are present
+    async fn handle_update(&self, update: Update) {
+        match update.update_kind {
+            UpdateKind::NewMessage(message) => {
+                if let Some(message_text) = &message.text
+                    && let Some(entities) = &message.entities
+                {
+                    for entity in entities {
+                        if let MessageEntityType::BotCommand = entity.entity_type {
+                            let command_text = &message_text[entity.offset as usize + 1
+                                ..(entity.offset + entity.length) as usize];
+                            if let Some(command_handler) = self.commands.get(command_text) {
+                                trace!(
+                                    "Handling command {}",
+                                    format!("/{}", command_text).yellow()
+                                );
+                                command_handler(CommandContext::new(self.client.clone(), message))
+                                    .await;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            _ => {}
         }
     }
 }
