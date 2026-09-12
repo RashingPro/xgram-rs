fb8d1a8e47624e279fb4e0101e7412199e88ba1d
RashingPro <rashingpro@yandex.ru>
2026-09-12 18:15:53 +0300

    feat: create `xgram::prelude` export

--- crates/xgram/src/lib.rs
+++ crates/xgram/src/lib.rs
@@ -4,3 +4,10 @@ pub mod command;
 pub use xgram_proc as proc;
 pub use xgram_telegram_api as telegram_api;
 pub use xgram_utils as utils;
+
+pub mod prelude {
+    pub use crate::bot::Bot;
+    pub use crate::command::context::CommandContext;
+    pub use crate::proc::*;
+    pub use crate::utils::config::BotConfig;
+}
